b5cbe034155df911f2f63cc0b5b2b30f1b791d31
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-02 15:07:37 -0700

    added background image and changed title/favicon

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   const [slogans, setSlogans] = useState(['The City of Roses', 'The city that never sleeps']);
 
   return (
-    <div className="App" style={{ backgroundImage: '../public/background.svg' }}>
+    <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
       <City
         skylineId={skylineId}
