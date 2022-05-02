1db97934e67efdbf41ead9aef568c85691d8f898
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-02 12:02:46 -0700

    set initial state for cityName and slogans

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
   const [waterfrontId, setWaterfrontId] = useState(1);
   const [castleId, setCastleId] = useState(1);
   // you'll need to track a city name, which starts as the city name of your choice.
+  const [cityName, setCityName] = useState('');
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
-
+  const [slogans, setSlogans] = useState(['The City of Roses', 'The city that never sleeps']);
   return (
     <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
